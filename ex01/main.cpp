953be0628a427d0f50c257a34c0a82acbe57fe25
mbmb-mbmb <markus.bonsdorff@iki.fi>
2025-08-07 15:20:52 +0300

    ex01 wip

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,23 +3,22 @@
 
 int main (int ac, char **av)
 {
-	if (ac != 2)
+	if (ac != 1)
 	{
 		std::cout << "too many argumnets or something" << std::endl;
 		return(0);
 	}
-	std::string input = av[1];
-
-	if(input == "ADD")
-		std::cout << "add" << std::endl;
-	else if (input == "SEARCH")	
-	std::cout << "search" << std::endl;
-	else if (input == "EXIT")
+	std::string input;
+	while(input != "EXIT")
 	{
-		std::cout << "exiting" << std::endl;
-		exit(0);
+		std::cout<<" blaabla?" << std::endl;
+		std::getline(std::cin, input);
+		if(input == "ADD")
+			std::cout << "add" << std::endl;
+		else if (input == "SEARCH")	
+			std::cout << "search" << std::endl;
+		else
+			std::cout << "nothing" << std::endl;
 	}
-	else
-		std::cout << "nothing" << std::endl;
 	return (0);
 }
